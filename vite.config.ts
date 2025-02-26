70361d49aa8757c5cade0f8094e8d15d3acae70c
yasminh-dev <yherr.dev@gmail.com>
2025-02-26 00:32:24 -0500

    adding ./build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: './build',
+  },
   plugins: [react()],
   server: {
     port: 8080,
